da52f29e19be68f456ff2ae2d5988fd53a0cb014
triborg34 <aref.mousavi0914@gmail.com>
2023-03-13 23:07:06 +0330

    back

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -38,45 +38,45 @@ class AudioVaribales extends GetxController {
 
 class RsssFeed extends GetxController {
   List<PODMODEL> podmodel = <PODMODEL>[].obs;
-  // var url =
-  //     'https://script.googleusercontent.com/macros/echo?user_content_key=FuWQ_R6ib9iBKVGSaeevC4ZSKADxIuodXrOQaUfXuhnW1v_qQqPJAXiF0zoSCjBAS3ez3JVq1f5t_HR_Ym1-5MQ7CzjYNwBGm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnKkSEnlfjz5sT8x_iEw_0zt61ju49Afbd3GS0UOc9VeSA2NnoPnaxRncIYi9itirh0KR95fEXcBg66Swx1_4g0rbeRigJsulYQ&lib=MQj4q2OlCXgAc-Qe0XPr3YHbQOj2qHGcZ';
+  var url =
+      'https://script.googleusercontent.com/macros/echo?user_content_key=FuWQ_R6ib9iBKVGSaeevC4ZSKADxIuodXrOQaUfXuhnW1v_qQqPJAXiF0zoSCjBAS3ez3JVq1f5t_HR_Ym1-5MQ7CzjYNwBGm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnKkSEnlfjz5sT8x_iEw_0zt61ju49Afbd3GS0UOc9VeSA2NnoPnaxRncIYi9itirh0KR95fEXcBg66Swx1_4g0rbeRigJsulYQ&lib=MQj4q2OlCXgAc-Qe0XPr3YHbQOj2qHGcZ';
     
-  //   void getData() async{
-  //     var response=await Dio().get(url).then((value){
+    void getData() async{
+      var response=await Dio().get(url).then((value){
         
 
-  //      for(var element in value.data['data']){
-  //       podmodel.add(PODMODEL(title: element['title'], audiourl:element['mp3url'] , imgurl: element['imgurl']));
-  //      }
+       for(var element in value.data['data']){
+        podmodel.add(PODMODEL(title: element['title'], audiourl:element['mp3url'] , imgurl: element['imgurl']));
+       }
 
-  //     });
-  //   }
+      });
+    }
 
-var url =
-      "https://rss.castbox.fm/everest/bbcc3f95966546cfb013c142326f1a59.xml";
+// var url =
+//       "https://rss.castbox.fm/everest/bbcc3f95966546cfb013c142326f1a59.xml";
 
-  void getData() async {
-    // ignore: unused_local_variable
-    var response = await Dio()
-        .get(
-      url,
-    )
-        .then((value) {
-      var rssfeed = RssFeed.parse(value.data);
-      for (var i in rssfeed.items!) {
-        // print(i.itunes!.image!.href);
-        // print(i.title);
-        // print(i.enclosure!.url);
+//   void getData() async {
+//     // ignore: unused_local_variable
+//     var response = await Dio()
+//         .get(
+//       url,
+//     )
+//         .then((value) {
+//       var rssfeed = RssFeed.parse(value.data);
+//       for (var i in rssfeed.items!) {
+//         // print(i.itunes!.image!.href);
+//         // print(i.title);
+//         // print(i.enclosure!.url);
 
-        podmodel.add(PODMODEL(
-          // desception: i.description,
-            title: i.title,
-            audiourl: i.enclosure!.url,
-            imgurl: i.itunes!.image!.href));
-      }
-    });
+//         podmodel.add(PODMODEL(
+//           // desception: i.description,
+//             title: i.title,
+//             audiourl: i.enclosure!.url,
+//             imgurl: i.itunes!.image!.href));
+//       }
+//     });
 
-  }
+//   }
   
 }
 
